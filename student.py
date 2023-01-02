3add701ae0c0b0ed62bb2c6bce4b9e8687c0ca5e
ocassidydev <ocassidy@tcd.ie>
2023-01-02 02:21:38 +0000

    Finished lesson

---
+++ student.py
@@ -0,0 +1,38 @@
+from datetime import date, timedelta
+import requests
+
+
+class Student:
+    """A Student class as base for method testing"""
+
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
+        self.naughty_list = False
+
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return "".join([f"{self._first_name.lower()}.",
+                       f"{self._last_name.lower()}@email.com"])
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get("".join(["http://company.com/course-schedule/",
+                                         f"{self._last_name}/",
+                                         f"{self._first_name}"]))
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
